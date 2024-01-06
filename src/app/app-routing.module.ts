a4be0295651f3d1dcd9f36e97f6abdec5ad43e6d
Amaya-2001 <112056360+Amaya-2001@users.noreply.github.com>
2024-01-06 18:50:25 +0530

    added the login component and created the login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [{ path: 'grama-niladhari', loadChildren: () => import('./grama-niladhari/grama-niladhari.module').then(m => m.GramaNiladhariModule) }];
+const routes: Routes = [
+  { path: 'grama-niladhari', loadChildren: () => import('./grama-niladhari/grama-niladhari.module').then(m => m.GramaNiladhariModule) },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
