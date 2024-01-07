4e79b0c1d995ab51fcfddf8ee9b9b2867ec0b3cc
Amaya-2001 <112056360+Amaya-2001@users.noreply.github.com>
2024-01-07 12:30:43 +0530

    write the onLogin method

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+ loginForm!:FormGroup
+  constructor(private fb:FormBuilder, private auth:AuthService, private router:Router) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email:['', [Validators.required, Validators.email]],
+      password:['', Validators.required]
+    })
+    
+  }
+  onLogin(){
+
+  }
 
 }
