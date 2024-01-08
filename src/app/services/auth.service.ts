cd72d7f92e83eb421e3c1fc3fb040658f5a083b3
Amaya-2001 <112056360+Amaya-2001@users.noreply.github.com>
2024-01-08 15:41:03 +0530

    added env file

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-private baseUrl:string = "https://localhost:7102/api/GramaNiladhari/";
+private baseUrl:string = environment.apiUrl;
 constructor(private http:HttpClient, private router:Router) { }
 
 signup(signupObj:any){
