b74bb4d64e5727ba8522ca56e4555e98f9afe940
Amaya-2001 <112056360+Amaya-2001@users.noreply.github.com>
2024-01-16 08:58:34 +0530

    Added sidebar  to the dashboard

--- src/app/grama-niladhari/grama-niladhari-routing.module.ts
+++ src/app/grama-niladhari/grama-niladhari-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { GramaNiladhariComponent } from './grama-niladhari.component';
 import { GnOnboardingComponent } from './components/onboarding/gn-onboarding/gn-onboarding.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/dashboard/sidebar/sidebar/sidebar.component';
 
 const routes: Routes = [{ path: '', component: GramaNiladhariComponent },
 { path: 'onboarding', component: GnOnboardingComponent },
-{ path: 'home', component:NavbarComponent}
+{ path: 'home', component:SidebarComponent}
 ];
 
 @NgModule({
