cc71643d5d76b780711269ad5cc96a2dcc388f28
Amaya-2001 <112056360+Amaya-2001@users.noreply.github.com>
2024-02-03 22:12:44 +0530

    Added options arrays to the select input forms. (#13)
    * created certificates component
    
    * create new certificate component and added routing to the sidebar
    
    * added route to the certificate page
    
    * Created the custome input form.
    
    * Add some of input fields to test the certificates page
    
    * Added some input field s to the certificates page
    
    * Added the reusable file input and input forms to the certificate page
    
    * Added reusable button to the certificate page.
    
    * Added resuable btn
    
    * Added the reusable check-box component
    
    * Changed the checkbox bg color
    
    * Updated the file input height
    
    * Updated the certificate page style
    
    * Fixed indentation and formatting
    
    * fixed formatting and indentation
    
    * Added gender option array to the select input
    
    * Added durationOptions array
    
    * Added the select options for civil status
    
    * Added options array for the select input forms
    
    * added event to handle checkbox
    
    * Connected the NestJS backend

--- src/app/grama-niladhari/components/certificates/certificates.component.ts
+++ src/app/grama-niladhari/components/certificates/certificates.component.ts
@@ -1,14 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-
+import {genderOptions,
+        durationOptions,
+        wSLOptions,
+        religonOptions,
+        districtOptions,
+        civilStatusOptions,
+        certificateType,
+        characterOptions,
+        sociableOptions,
+        lawOptions,
+        divisionalSecretariatsDivOptions,
+        gnDivisionNameOptions,
+        gnDivisionNumberOptions
+        } from '../../select.input.options/options';
+import { AuthService } from '../../../services/auth.service';
+import { NgToastService } from 'ng-angular-popup';
 @Component({
   selector: 'app-certificates',
   templateUrl: './certificates.component.html',
   styleUrls: ['./certificates.component.css']
 })
 export class CertificatesComponent implements OnInit {
-
-  constructor() { }
+  isChecked: boolean = false;
+  genderOptions = genderOptions;
+  durationOptions = durationOptions;
+  wSLOptions = wSLOptions;
+  religonOptions = religonOptions;
+  districtOptions = districtOptions;
+  civilStatusOptions = civilStatusOptions;
+  certificateType = certificateType;
+  characterOptions = characterOptions;
+  sociableOptions = sociableOptions;
+  lawOptions = lawOptions;
+  divisionalSecretariatsDivOptions = divisionalSecretariatsDivOptions;
+  gnDivisionNameOptions = gnDivisionNameOptions;
+  gnDivisionNumberOptions = gnDivisionNumberOptions;
+  constructor(private auth:AuthService, private toast:NgToastService) { }
 
   ngOnInit() {
   }
@@ -16,30 +44,51 @@ export class CertificatesComponent implements OnInit {
   formGroup = new FormGroup({
     certificateType: new FormControl('', Validators.required),
     district: new FormControl('', Validators.required),
-    division: new FormControl('', Validators.required),
-    duration: new FormControl('', Validators.required),
-    divisionId: new FormControl('', Validators.required),
+    divisionalSecretariatsDiv: new FormControl('', Validators.required),
+    personallyKnown: new FormControl('', Validators.required),
+    sinceWhenKnown: new FormControl('', Validators.required),
+    gramaNiladhariDivName: new FormControl('', Validators.required),
+    gramaNiladhariDivNum: new FormControl('', Validators.required),
     fullName: new FormControl('', Validators.required),
     gender: new FormControl('', Validators.required),
     address: new FormControl('', Validators.required),
     nic: new FormControl('', Validators.required),
     age: new FormControl('', Validators.required),
-    civilstatus: new FormControl('', Validators.required),
-    wSL: new FormControl('', Validators.required),
+    civilStatus: new FormControl('', Validators.required),
+    isSriLankan: new FormControl('', Validators.required),
     religion: new FormControl('', Validators.required),
     occupation: new FormControl('', Validators.required),
     periods: new FormControl('', Validators.required),
-    electrol: new FormControl('', Validators.required),
-    fatherDetails: new FormControl('', Validators.required),
-    purpose: new FormControl('', Validators.required),
+    electoralRegisterNumber: new FormControl('', Validators.required),
+    nameOfFather: new FormControl('', Validators.required),
+    addressOfFather: new FormControl('', Validators.required),
+    purposeOfCertificate: new FormControl('', Validators.required),
     periodsOfResidence: new FormControl('', Validators.required),
     proof: new FormControl('', Validators.required),
-    law: new FormControl('', Validators.required),
+    convictedByCourtOfLaw: new FormControl('', Validators.required),
     character: new FormControl('', Validators.required),
-    sociable: new FormControl('', Validators.required),
-    remarks: new FormControl('', Validators.required),
-    gnSign: new FormControl('', Validators.required),
-    applicantSign: new FormControl('', Validators.required)
+    takenPartInPublicActivities: new FormControl('', Validators.required),
+    remarks: new FormControl(''),
+    // gnSign: new FormControl('', Validators.required),
+    // applicantSign: new FormControl('', Validators.required)
   })
+
+  handleCheckBoxChange(event: any) {
+    this.isChecked = event.target.checked;
+  }
+  onGenerateCertificate(){
+    console.log(this.formGroup.value);
+    this.auth.generateCertificate(this.formGroup.value).subscribe({
+      next:(res)=>{
+        this.toast.success({detail:"Success!",summary:res.message, duration:5000})
+        this.formGroup.reset();
+
+      },
+      error:(err)=>{
+        this.toast.error({detail:"Error!",summary:"Something went wrong!", duration:5000})
+      }
+    })
+
+  }
 
 }
---
+++ src/app/grama-niladhari/select.input.options/options.ts
@@ -0,0 +1,83 @@
+export const genderOptions = [
+    { value: '1', options: 'MALE' },
+    { value: '2', options: 'FEMALE' },
+]
+export const durationOptions = [
+    { value: '1', options: 'Childhood' },
+    { value: '2', options: 'Adult' },
+    { value: '3', options: '20 years ago' },
+    { value: '4', options: '50 years ago' },
+]
+export const wSLOptions=[
+    { value: '1', options: 'YES' },
+    { value: '2', options: 'NO' },
+]
+export const religonOptions=[
+    { value: '1', options: 'BUDDHISM' },
+    { value: '2', options: 'HINDUISM' },
+    { value: '3', options: 'ISLAM' },
+    { value: '4', options: 'CHRISTIANITY' },
+    { value: '5', options: 'OTHER' },
+]
+export const districtOptions = [
+    { value: '1', options: 'AMPARA' },
+    { value: '2', options: 'ANURADHAPURA' },
+    { value: '3', options: 'BADULLA' },
+    { value: '4', options: 'BATTICALOA' },
+    { value: '5', options: 'COLOMBO' },
+    { value: '6', options: 'GALLE' },
+    { value: '7', options: 'GAMPAHA' },
+    { value: '8', options: 'HAMBANTOTA' },
+    { value: '9', options: 'JAFFNA' },
+    { value: '10', options: 'KALUTARA' },
+    { value: '11', options: 'KANDY' },
+    { value: '12', options: 'KEGALLE' },
+    { value: '13', options: 'KILINOCHCHI' },
+    { value: '14', options: 'KURUNEGALA' },
+    { value: '14', options: 'MANNAR' },
+    { value: '15', options: 'MATALE' },
+    { value: '16', options: 'MATARA' },
+    { value: '17', options: 'MONERAGALA' },
+    { value: '18', options: 'MULLAITIVU' },
+    { value: '19', options: 'NUWARA_ELIYA' },
+    { value: '20', options: 'POLONNARUWA' },
+    { value: '21', options: 'PUTTALAM' },
+    { value: '22', options: 'RATNAPURA' },
+    { value: '23', options: 'TRINCOMALEE' },
+    { value: '24', options: 'VAVUNIYA' },
+]
+export const civilStatusOptions = [
+    { value: '1', options: 'MARRIED' },
+    { value: '2', options: 'DIVORCED' },
+    { value: '3', options: 'NOT_MARRIED' },
+    { value: '4', options: 'WIDOWED' },
+]
+export const certificateType = [
+    { value: '1', options: 'Certificate on residence and character issued by the Grama Niladhari' },
+]
+export const characterOptions=[
+    { value: '1', options: 'Very Good' },
+    { value: '2', options: 'Good' },
+    { value: '3', options: 'Bad' },
+    { value: '4', options: 'Very Bad' },
+]
+export const sociableOptions=[
+    { value: '1', options: 'YES' },
+    { value: '2', options: 'NO' },
+]
+export const lawOptions=[
+    { value: '1', options: 'YES' },
+    { value: '2', options: 'NO' },
+]
+export const divisionalSecretariatsDivOptions=[
+    { value: '1', options: 'Panadura' },
+    { value: '2', options: 'Kalutara' },
+]
+export const gnDivisionNameOptions=[
+    { value: '1', options: 'Melgama south' },
+    { value: '2', options: 'Rathnapura' },
+]
+export const gnDivisionNumberOptions=[
+    { value: '1', options: '703/B' },
+    { value: '2', options: '101' },
+]
